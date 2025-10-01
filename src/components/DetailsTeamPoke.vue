a79d38dda60b52a2bd25d4107bbfa4a1515cde6c
yani-tolaba <yanina.tolaba@mjus.gba.gob.ar>
2025-10-01 18:59:48 -0300

    fix letra de container detalle

--- src/components/DetailsTeamPoke.vue
+++ src/components/DetailsTeamPoke.vue
@@ -7,7 +7,7 @@ const team = useTeamStore()
 
 <template>
     <div v-if="team.team.length > 0" class="border border-white rounded-3xl mx-4 mt-10">
-        <h1 v-if="team.team.length > 0" class="text-2xl font-bold text-left p-4">Detalles de mi equipo Pokémon</h1>
+        <h1 v-if="team.team.length > 0" class="text-2xl font-bold text-left p-4 text-white">Detalles de mi equipo Pokémon</h1>
         <div class="items-center grid grid-cols-4">
             <div v-for="poke in team.team" :key="poke.id">
                 <ShowPoke :pokemon="poke" :isLoading="false" :inTeam="true" class="max-w-sm mx-4 p-4 bg-white/85 shadow-lg bg-opacity-25 mb-4" />
